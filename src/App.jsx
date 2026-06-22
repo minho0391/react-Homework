84046c7ae6bd3b56d4eefb6dd73aa960561de4e0
minho0391 <minh05@naver.com>
2026-06-22 17:22:36 +0900

    feat: pass product props to card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  return <div>React Mission</div>;
+  const product = {
+    name: "무선 키보드",
+    price: 39000,
+    brand: "EZTECH",
+  };
+
+  const { name, price, brand } = product;
+
+  return (
+    <div>
+      <Card name={name} price={price} brand={brand} />
+    </div>
+  );
 }
 
 export default App;
